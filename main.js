7f9d6b67239ba5b269140770bd7c576e61e7ee74
Abdulrahuman <132744501+AbdulRahumankm@users.noreply.github.com>
2024-07-07 21:29:15 +0530

    route

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 import express from "express";
+import movieRoutes from "./routes/movies.route.js"
 
 const app = express();
 const PORT = 3030;
@@ -7,6 +8,9 @@ app.get("/", (req, res) => {
   res.json({ msg: "Hello World!" });
 });
 
+app.use('/movies', movieRoutes)
+
+
 app.listen(PORT, () => {
-  console.log(`This server is running at http://localhost ${PORT}`);
+  console.log(`This server is running at http://localhost:${PORT}`);
 });
---
+++ routes/movies.route.js
@@ -0,0 +1,25 @@
+import express from "express"
+
+const router = express.Router();
+
+//R - For Reading
+router.get('/movies' , (req , res)=>{
+
+})
+
+//C - For Create
+router.post('/movies' , (req , res)=>{
+
+})
+
+//U - For Update
+router.put('/movies/:id' , (req , res)=>{
+
+})
+
+//D - For Delete
+router.delete('/movies/:id' , (req , res)=>{
+
+})
+
+export default router;
