adefd5b29a60c5b7f1dd1d4a3901b7d76fb1876e
Abdulrahuman <132744501+AbdulRahumankm@users.noreply.github.com>
2024-07-07 21:43:19 +0530

    node changes

--- routes/movies.route.js
+++ routes/movies.route.js
@@ -1,25 +1,25 @@
-import express from "express"
+import express from "express";
 
 const router = express.Router();
 
 //R - For Reading
-router.get('/movies' , (req , res)=>{
-
-})
+router.get("/movies", (req, res) => {
+  res.send("Get all movie lists");
+});
 
 //C - For Create
-router.post('/movies' , (req , res)=>{
-
-})
+router.post("/movies", (req, res) => {
+    res.send("Create a movie");
+});
 
 //U - For Update
-router.put('/movies/:id' , (req , res)=>{
-
-})
+router.put("/movies/:id", (req, res) => {
+    res.send("update a movie");
+});
 
 //D - For Delete
-router.delete('/movies/:id' , (req , res)=>{
-
-})
+router.delete("/movies/:id", (req, res) => {
+    res.send("delete a movie");
+});
 
 export default router;
