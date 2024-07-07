ae9a0dcdfcaecefe5e2ea737d9c359f45cb4a797
Abdulrahuman <132744501+AbdulRahumankm@users.noreply.github.com>
2024-07-07 21:49:28 +0530

    router

--- routes/movies.route.js
+++ routes/movies.route.js
@@ -3,22 +3,22 @@ import express from "express";
 const router = express.Router();
 
 //R - For Reading
-router.get("/movies", (req, res) => {
+router.get("/", (req, res) => {
   res.send("Get all movie lists");
 });
 
 //C - For Create
-router.post("/movies", (req, res) => {
+router.post("/", (req, res) => {
     res.send("Create a movie");
 });
 
 //U - For Update
-router.put("/movies/:id", (req, res) => {
+router.put("/:id", (req, res) => {
     res.send("update a movie");
 });
 
 //D - For Delete
-router.delete("/movies/:id", (req, res) => {
+router.delete("/:id", (req, res) => {
     res.send("delete a movie");
 });
 
