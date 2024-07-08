68bc9af2ef2420bf437c1cb5b964e69bb1a28361
Abdulrahuman <132744501+AbdulRahumankm@users.noreply.github.com>
2024-07-08 11:28:56 +0530

    check spell

--- controllers/movies.controller.js
+++ controllers/movies.controller.js
@@ -1,39 +1,35 @@
 import Movie from "../models/movie.model.js";
 
-export const movieIndex=  (req, res) => {
-    res.send("Get all movie lists");
+export const movieIndex = (req, res) => {
+  res.send("Get all movie lists");
 };
 
-export const create = async(req, res) => {
-    // id , tittle , desc
-    // console.log(req.body);
-    //return res.json(req.body)
-    //create the movie info 
+// Create a movie
+export const create = async (req, res) => {
+  // id , tittle , desc
+  // console.log(req.body);
+  //return res.json(req.body)
+  //create the movie info
 
-   const newMovie = new Movie({
-       title: req.body.title,
-       desc : req.body.desc,
-    })
+  const newMovie = new Movie({
+    // check the spell
+    title: req.body.title,
+    desc: req.body.desc,
+  });
+  console.log(req.body);
 
-    try {
-        const movie = await newMovie.save();
-        return res.status(201).json(movie)
-    } catch (error) {
-        return res.status(400).json({message:error.message})
-    }
-
-   
-
-   
+  try {
+    const movie = await newMovie.save();
+    return res.status(201).json(movie);
+  } catch (error) {
+    return res.status(400).json({ message: error.message });
+  }
 };
 
 export const update = (req, res) => {
-    res.send("update a movie");
+  res.send("update a movie");
 };
 
-export const remove =  (req, res) => {
-    res.send("delete a movie");
+export const remove = (req, res) => {
+  res.send("delete a movie");
 };
-
-
-
